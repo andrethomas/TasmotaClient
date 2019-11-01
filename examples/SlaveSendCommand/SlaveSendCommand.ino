8d25ceecc9e819cb940ebf0d761425de22f2034c
andrethomas <andre1024@gmail.com>
2019-11-01 23:34:24 +0200

    Bugfix for A pins

--- examples/SlaveSendCommand/SlaveSendCommand.ino
+++ examples/SlaveSendCommand/SlaveSendCommand.ino
@@ -24,7 +24,7 @@ void user_FUNC_JSON(void)
 {
   uint8_t a = 0;
   char myjson[100];
-  sprintf(myjson,"{\"A0\": %u, \"A0\": %u, \"A0\": %u, \"A0\": %u, \"A0\": %u, \"A0\": %u, \"A0\": %u, \"A0\": %u}", analogRead(A0), analogRead(A1), analogRead(A2), analogRead(A3), analogRead(A4), analogRead(A5), analogRead(A6), analogRead(A7));
+  sprintf(myjson,"{\"A0\": %u, \"A1\": %u, \"A2\": %u, \"A3\": %u, \"A4\": %u, \"A5\": %u, \"A6\": %u, \"A7\": %u}", analogRead(A0), analogRead(A1), analogRead(A2), analogRead(A3), analogRead(A4), analogRead(A5), analogRead(A6), analogRead(A7));
   slave.sendJSON(myjson);
 }
 
